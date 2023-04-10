42c80634f0b0f2129952359f3db7c9db0c57af57
William Harrison <william@williamdavidharrison.com.au>
2023-04-10 16:05:25 +0800

    feat: add setup script + rename exe

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 A simple `cmd://` protocol which runs commands in your terminal through your browser.
 
 ## 🖱️ Setting Up
-1. Download the [executable](https://raw.githubusercontent.com/WilliamDavidHarrison/cmd-protocol/main/Command%20Executor.exe) and [register](https://raw.githubusercontent.com/WilliamDavidHarrison/cmd-protocol/main/register.reg) files.
-2. Open and run the `register.reg` file.
-3. Move the `Command Executor.exe` file to a new folder in `C:\Protocols\cmd`.
-    - The file path of the executable should be `C:\Protocols\cmd\Command Executor.exe`.
-4. Try it out!
-    - In your browser, in the URL bar, type something like `cmd://help` and run the protocol.
+1. Download the [repository](https://github.com/cmd-protocol/cmd-protocol/archive/refs/heads/main.zip) files.
+2. Extract the zip file.
+3. Open the extracted folder and then click on `cmd-protocol-main` folder.
+4. Run the `setup.bat` file and when asked for Administrator privileges, click `Yes`.
+    - The batch file registers the registry values stored in `register.reg` and copies the `cmd.exe` file to the required file location.
+5. Try it out!
+    - In your browser, in the URL bar, type something like [`cmd://help`](cmd://help) and run the protocol.
 
 ## ⚠️ Warning
 Websites may use this protocol maliciously, so if you ever get a pop-up for it when you didn't run it voluntarily, click `Cancel` so the command doesn't run.
---
+++ setup.bat
@@ -0,0 +1,76 @@
+@echo off
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+if exist cmd.exe (
+  echo [32mCHECK[0m cmd.exe exists
+) else (
+  echo [91mCHECK[0m cmd.exe does not exist
+  echo:
+
+  pause
+  exit
+)
+
+if exist register.reg (
+  echo [32mCHECK[0m register.reg exists
+) else (
+  echo [91mCHECK[0m register.reg does not exist
+  echo:
+
+  pause
+  exit
+)
+
+echo:
+
+echo [33mAdding required values to registry...[0m
+regedit.exe /S register.reg >nul
+echo [32mAdded required values to the registry.[0m
+
+echo:
+
+echo [33mCopying "cmd.exe" to "%HOMEDRIVE%\Protocols\cmd.exe"...[0m
+xcopy "cmd.exe" "%HOMEDRIVE%\Protocols\" /E /C /H /R /K /O /Y >nul
+echo [32mCopied "cmd.exe" to "%HOMEDRIVE%\Protocols\cmd.exe".[0m
+
+echo:
+
+pause
